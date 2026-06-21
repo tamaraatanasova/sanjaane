3736e8c17d450134161459c1641614d11483b48d
Tamara <tamara@tamara.tamara>
2026-06-21 17:42:05 +0200

    1

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -3,16 +3,25 @@ import { initReactI18next } from 'react-i18next';
 import { mk, hr } from './locales';
 
 const getLanguageFromHostname = () => {
+  // Default za SSR / build environment
   if (typeof window === 'undefined') {
     return 'mk';
   }
 
   const hostname = window.location.hostname.toLowerCase();
 
+  // Hrvatski:
+  // hr.localhost
+  // hr.sanja-angelcho.com
   if (hostname.startsWith('hr.')) {
     return 'hr';
   }
 
+  // Makedonski:
+  // mk.localhost
+  // mk.sanja-angelcho.com
+  // sanja-angelcho.com
+  // www.sanja-angelcho.com
   return 'mk';
 };
 
@@ -20,13 +29,21 @@ i18n
   .use(initReactI18next)
   .init({
     resources: {
-      mk: { translation: mk },
-      hr: { translation: hr },
+      mk: {
+        translation: mk,
+      },
+      hr: {
+        translation: hr,
+      },
     },
+
     lng: getLanguageFromHostname(),
     fallbackLng: 'mk',
     supportedLngs: ['mk', 'hr'],
-    interpolation: { escapeValue: false },
+
+    interpolation: {
+      escapeValue: false,
+    },
   });
 
 export default i18n;
